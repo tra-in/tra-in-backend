d2d7910172881ce357ddc91b8dad8ca0824a16ef
sseooh <sseooh@gmail.com>
2025-12-16 01:08:02 +0900

    feat: ai 추천 장소 db 저장

--- train/src/main/java/com/example/train/controller/UserPickController.java
+++ train/src/main/java/com/example/train/controller/UserPickController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/user-picks")
+@RequestMapping("/api/user-picks")
 @RequiredArgsConstructor
 public class UserPickController {
 
